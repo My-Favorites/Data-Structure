0c9b41c45a3d1871b4c532e7a0332ac93cd855a3
Zeno Zeng <zenoofzeng@gmail.com>
2015-09-30 19:22:12 +0800

    rewrite test bash script

--- src/fz/project1/test.sh
+++ src/fz/project1/test.sh
@@ -2,14 +2,13 @@
 
 # Bash Script for Testing
 
-echo ">> O(n^6)"
-gcc on6.c test.c -std=c99 -o a.out
-./a.out
+function testCode {
+    echo # print an empty line
+    echo -n ">> $1"
+    gcc $2 test.c -std=c99 -o a.out
+    ./a.out
+}
 
-echo ">> O(n^4)"
-gcc on4.c test.c -std=c99 -o a.out
-./a.out
-
-echo ">> Bonus O(n^3)"
-gcc bonus1.c test.c -std=c99 -o a.out
-./a.out
+testCode "O(n^6)" on6.c
+testCode "O(n^4)" on4.c
+testCode "Bonus O(n^3)" bonus1.c
