9dd87603649a857fb38bc2e7bfaed27acf614d9d
sunoru <s@sunoru.com>
2016-01-16 13:27:43 +0800

    Bonus 2

--- src/sunoru/1673-universal-travel-sites.c
+++ src/sunoru/1673-universal-travel-sites.c
@@ -1,110 +1,92 @@
 #include<stdio.h>
 #include<string.h>
+#define MAXN 1010
+#define oo 200701281
+
+char tstr[3];
+int m, n;
+int src, dest;
+char dict[MAXN][3];
+int map[MAXN][MAXN];
+int head, tail;
+int queue[MAXN];
+int d[MAXN], e[MAXN];
 
 int
-main(void)
+find(char x[3])
 {
-    char a[5], b[5], tou[5], wei[5], ccc, c[1010][5];
-    int n, i, j, h, m, k1, k2, l, tt, ww, head, tail, d[1010], x, y;
-    long f[1010][1010], line[2000], max, money, e[1010];
-
-    for (i = 1; i <= 4; i++)
-        scanf("%c", &tou[i]);
-    for (i = 1; i <= 4; i++)
-        scanf("%c", &wei[i]);
-    scanf("%d%c", &n, &ccc);
-
-    m = 0;
-
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= 4; j++)
-            scanf("%c", &a[j]);
-        k1 = 0;
-        for (l = 1; l <= m; l++)
-            if (c[l][1] == a[1] && c[l][2] == a[2] && c[l][3] == a[3]) {
-                k1 = l;
-                break;
-            }
-        if (k1 == 0) {
-            m++;
-            k1 = m;
-            c[k1][1] = a[1];
-            c[k1][2] = a[2];
-            c[k1][3] = a[3];
-        }
-        for (j = 1; j <= 4; j++)
-            scanf("%c", &b[j]);
-        k2 = 0;
-        for (l = 1; l <= m; l++)
-            if (c[l][1] == b[1] && c[l][2] == b[2] && c[l][3] == b[3]) {
-                k2 = l;
-                break;
-            }
-        if (k2 == 0) {
-            m++;
-            k2 = m;
-            c[k2][1] = b[1];
-            c[k2][2] = b[2];
-            c[k2][3] = b[3];
-        }
-        scanf("%d%c", &h, &ccc);
-        f[k1][k2] = h;
+    int i;
+    for (i = 0; i < n; ++i) {
+        if (x[0] == dict[i][0] &&
+            x[1] == dict[i][1] &&
+            x[2] == dict[i][2])
+            return i;
     }
-    for (l = 1; l <= m; l++)
-        if (c[l][1] == tou[1] && c[l][2] == tou[2] && c[l][3] == tou[3]) {
-            tt = l;
-            break;
-        }
-    for (l = 1; l <= m; l++)
-        if (c[l][1] == wei[1] && c[l][2] == wei[2] && c[l][3] == wei[3]) {
-            ww = l;
-            break;
-        }
+    dict[n][0] = x[0];
+    dict[n][1] = x[1];
+    dict[n][2] = x[2];
+    return n++;
+}
 
-    money = 0;
+int
+main()
+{
+    char ssrc[3], sdest[3];
+    int a, b;
+    int u, v;
+    int i;
+    int s = 0;
+    scanf("%s%s%d", ssrc, sdest, &m);
 
-    while (1) {
-        if (tt == ww)
-            break;
-        head = 1;
-        tail = 1;
-        line[head] = tt;
-        memset(d, 0, sizeof(d));
-        memset(e, 0, sizeof(e));
-        d[tt] = -1;
-        e[tt] = 1000000;
-
-        while (head <= tail) {
-            x = line[head];
-            if (x == ww)
-                break;
-            for (j = 1; j <= m; j++)
-                if (f[x][j] > 0 && d[j] == 0) {
-                    d[j] = x;
-                    if (f[x][j] < e[x])
-                        e[j] = f[x][j];
-                    else
-                        e[j] = e[x];
-                    tail++;
-                    line[tail] = j;
-                }
-            head++;
-        }
-
-        if (e[ww] == 0)
-            break;
-        money = money + e[ww];
-        y = ww;
-        while (y != tt) {
-            x = d[y];
-            f[x][y] = f[x][y] - e[ww];
-            f[y][x] = f[y][x] + e[ww];
-            y = x;
-        }
+    n = 0;
+    for (i = 0; i < m; ++i) {
+        scanf("%s", tstr);
+        a = find(tstr);
+        scanf("%s", tstr);
+        b = find(tstr);
+        scanf("%d", &map[a][b]);
     }
-
-    printf("%ld\n", money);
+    src = find(ssrc);
+    dest = find(sdest);
     
+    while (src != dest) {
+        head = 0;
+        tail = 0;
+        queue[tail++] = src;
+        for (i = 0; i < n; ++i)
+            d[i] = -1;
+        for (i = 0; i < n; ++i)
+            e[i] = 0;
+        d[src] = -2;
+        e[src] = oo;
+        while (head != tail) {
+            u = queue[head++];
+            if (u == dest)
+                break;
+            for (v = 0; v < n; ++v)
+                if (map[u][v] > 0 && d[v] == -1) {
+                    d[v] = u;
+                    if (map[u][v] < e[u])
+                        e[v] = map[u][v];
+                    else
+                        e[v] = e[u];
+                    queue[tail++] = v;
+                }
+        }
+        if (e[dest] == 0)
+            break;
+        s += e[dest];
+        u = dest;
+        while (u != src) {
+            v = d[u];
+            map[v][u] -= e[dest];
+            map[u][v] += e[dest];
+            u = v;
+        }
+    }
+
+    printf("%d\n", s);
+
     return 0;
 }
 
